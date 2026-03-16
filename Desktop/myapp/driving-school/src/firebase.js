53f94e4d7ef459744c9002e3e290be68f65730e8
Mardonbek <abdullaev07amf.se@MacBook-Air-Mardonbek.local>
2026-03-16 07:07:58 +0600

    fix firebase setup for vercel

--- Desktop/myapp/driving-school/src/firebase.js
+++ Desktop/myapp/driving-school/src/firebase.js
@@ -1,18 +1,14 @@
-import { initializeApp } from "firebase/app"
-import { getFirestore } from "firebase/firestore"
-import { getAuth } from "firebase/auth"
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCjF-EDmUY89q4tudpDFqG_TakmkcgZ2ow",
-  authDomain: "driving-school-1c9f6.firebaseapp.com",
-  projectId: "driving-school-1c9f6",
-  storageBucket: "driving-school-1c9f6.firebasestorage.app",
-  messagingSenderId: "521733819709",
-  appId: "1:521733819709:web:d17140a22f6deb9ef04954",
-  measurementId: "G-DQM7ZSG108"
-}
+  apiKey: "ТВОЙ_API_KEY",
+  authDomain: "ТВОЙ_AUTH_DOMAIN",
+  projectId: "ТВОЙ_PROJECT_ID",
+  storageBucket: "ТВОЙ_STORAGE_BUCKET",
+  messagingSenderId: "ТВОЙ_MESSAGING_SENDER_ID",
+  appId: "ТВОЙ_APP_ID"
+};
 
-const app = initializeApp(firebaseConfig)
-
-export const db = getFirestore(app)
-export const auth = getAuth(app)
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
